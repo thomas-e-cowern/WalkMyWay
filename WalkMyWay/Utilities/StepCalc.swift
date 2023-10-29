88e45aa33f1f190c49cc5e97ac997dcb6fba0f0f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-29 12:23:10 -0400

    add utilities folder and step calc file

---
+++ WalkMyWay/Utilities/StepCalc.swift
@@ -0,0 +1,16 @@
+//
+//  StepCalc.swift
+//  WalkMyWay
+//
+//  Created by Thomas Cowern on 10/29/23.
+//
+
+import Foundation
+
+func isUnder10000(_ count: Int) -> Bool {
+    count < 10000
+}
+
+func isUnder5000(_ count: Int) -> Bool {
+    count < 5000
+}
