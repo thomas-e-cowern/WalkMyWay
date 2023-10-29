21d66141f5a6a9a13c42b3bb1537997907f4032f
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-10-29 10:26:15 -0400

    walk my way initial commit

---
+++ WalkMyWay/WalkMyWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkMyWayApp.swift
+//  WalkMyWay
+//
+//  Created by Thomas Cowern on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WalkMyWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
